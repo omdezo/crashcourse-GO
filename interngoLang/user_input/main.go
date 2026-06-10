644d8539bddd6275552c5b405a6afaa7c9d4d607
omdezo <omaraflah9988@gmail.com>
2026-06-10 14:03:12 +0400

    new changes and todays courses

---
+++ interngoLang/user_input/main.go
@@ -0,0 +1,37 @@
+package main 
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader)(string,error){
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input),err
+
+}
+func CreateBill() Bill{
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+	name, _ := getInput("create a new bill name: ",reader)
+	b := newBill(name)
+	fmt.Println("create the bill - ", b.name)
+	return b
+}
+func promptOptions(b Bill){
+	reader := bufio.NewReader(os.Stdin)
+
+	opt,_ := getInput("choose option(a- add item, s- save bill, t - add tip)", reader)
+	fmt.Println(opt)
+}
+
+func main(){
+	mybill := CreateBill()
+	promptOptions(mybill)
+	fmt.Println(mybill)
+}
